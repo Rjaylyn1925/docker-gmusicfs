fcd92279b69d4bd8d326179eb4749b01bdc63887
digitalhigh <d8ahazard@gmail.com>
2017-05-03 09:28:45 -0500

    WE CAN DO EEEEET!

--- run.sh
+++ run.sh
@@ -6,3 +6,11 @@ docker run \
     --cap-add SYS_ADMIN \
     --device /dev/fuse \
     gmusicfs:1 /bin/bash
+
+echo "[credentials]" > ~/.gmucisfs
+echo "GOOGLE_USERNAME = " $GOOGLE_USERNAME >> ~/.gmucisfs
+echo "GOOGLE_PASSWORD = " $GOOGLE_PASSWORD  >> ~/.gmucisfs
+echo "GOOGLE_DEVICE_ID = " $GOOGLE_DEVICE_ID  >> ~/.gmucisfs
+chmod 600 ~/.gmusicfs
+echo "Starting gmusicFS!!"
+gmusicfs -t /root/music
